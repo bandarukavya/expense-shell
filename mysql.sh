ba67f63439e19040bcc557af188f7a95a23c67f8
bandarukavya <bandarukavya22@gmail.com>
2025-05-10 16:27:34 +0530

    Expensepro

--- mysql.sh
+++ mysql.sh
@@ -11,6 +11,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "Please enter DB password:"
+read mysql_root_password
+
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -42,10 +46,10 @@ VALIDATE $? "Starting MYSQL Server"
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 #VALIDATE $? "Setting up root password"
 
-mysql -h db.daws78sk.site -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.daws78sk.site -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
-    mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>$LOGFILE
     VALIDATE $? "MySql Root Password Setup"
 else
     echo -e "MySQL root password is already setup... $Y SKIPPING $N"
